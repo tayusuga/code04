eb2f1391a2a760a8bc89cbd2bcdfe6ce9ed3df96
tayusuga <sugatayusuke@net85-dhcp-146.sfc.keio.ac.jp>
2016-04-28 10:49:51 +0900

    write M

--- js/circle.js
+++ js/circle.js
@@ -1,7 +1,13 @@
 function R (length){
      var result;
-     result=Math.PI * length * 2
+     result=Math.PI * length * 2;
      
    
      return result;
+}
+
+function M (length){
+    var result;
+    result = length * length * Math.PI;
+    return result;
 }
